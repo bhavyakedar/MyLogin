6ef3211724c80ca86903ea11f88637f2fbc4cc9a
bhavyakedar <58164903+bhavyakedar@users.noreply.github.com>
2020-04-06 04:11:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mylogin/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.mylogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username,password;
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String user_name = username.getText().toString().trim();
+                String pass_word = password.getText().toString().trim();
+                login.setEnabled(!user_name.isEmpty() && !pass_word.isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        username.addTextChangedListener(textWatcher);
+        password.addTextChangedListener(textWatcher);
+    }
+}
